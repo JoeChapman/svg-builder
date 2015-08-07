c2fd59d3b9aa78c447d57723144d0755acb85ccf
Niek Oost <niekoost@gmail.com>
2015-08-07 20:21:20 +0200

    Import rect

--- elements/index.js
+++ elements/index.js
@@ -5,3 +5,4 @@ module.exports.Circle = require('./circle');
 module.exports.Text = require('./text');
 module.exports.ForeignObject = require('./foreignobject');
 module.exports.Line = require('./line');
+module.exports.Rect = require('./rect');
